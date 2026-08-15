3b6e3704e568af1fd697c19d5b29233124f9e2fb
Raghul-cmd <araghul775@gmail.com>
2026-08-15 18:17:58 +0530

    Update Supabase credentials in config.js

--- js/config.js
+++ js/config.js
@@ -9,8 +9,8 @@ export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiO
 
 // Indicates if live credentials have been configured
 export const isSupabaseConfigured = () => {
-    return SUPABASE_URL &&
-        !SUPABASE_URL.includes("https://kuajhwywwvjykxjaaxkg.supabase.co") &&
-        SUPABASE_ANON_KEY &&
-        !SUPABASE_ANON_KEY.includes("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt1YWpod3l3d3ZqeWt4amFheGtnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY3ODgzNDEsImV4cCI6MjEwMjM2NDM0MX0.hnjYcD2mfUuKzTp9ciLw5FfPp4xLj4p9RmScTgdE12k");
+    return SUPABASE_URL && 
+           !SUPABASE_URL.includes("YOUR_SUPABASE_PROJECT_ID") && 
+           SUPABASE_ANON_KEY && 
+           !SUPABASE_ANON_KEY.includes("YOUR_SUPABASE_PUBLISHABLE_KEY");
 };
